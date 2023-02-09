b2d7fa34f9a1394b41bdf49c88d75b924fcaf1ff
AHRomen <73189275+AHRomen@users.noreply.github.com>
2023-02-09 12:19:27 +0000

    js update

--- my.js
+++ my.js
@@ -1,5 +1,7 @@
 r = document.getElementById("right").innerHTML = 0;
 w = document.getElementById("wrong").innerHTML = 0;
+temp  = 0;
+
 document.getElementById("num1").innerHTML = Math.floor(Math.random()*10000);
 document.getElementById("num2").innerHTML = Math.floor(Math.random()*10000);
 var num1 =  document.getElementById("num1").innerHTML;
@@ -17,24 +19,35 @@ function next(){
         value = Number(value);
         document.getElementById("complement").innerHTML="";
 }
-function csum(){
-    var num1 =  document.getElementById("num1").innerHTML;
-        num1 = Number(num1);
-    var num2 =  document.getElementById("num2").innerHTML;
-        num2 = Number(num2);
-    var sum = num1 + num2;
-    var ans =  document.getElementById("ans").value;
+var ans =  document.getElementById("ans").value;
         ans = Number(ans)
-        if(sum == ans ){
-            document.getElementById("complement").innerHTML="Hurry! You are Right";
-                r= r+1;
-                document.getElementById("right").innerHTML = r;
-        }
-        else{
-            document.getElementById("complement").innerHTML="Opps! try Again";
-            w= w+1;
-            document.getElementById("wrong").innerHTML = w;
-        }
-}
+
+       
   
-    
+        function csum(){
+            var num1 =  document.getElementById("num1").innerHTML;
+                num1 = Number(num1);
+            var num2 =  document.getElementById("num2").innerHTML;
+                num2 = Number(num2);
+            var sum = num1 + num2;
+            var ans =  document.getElementById("ans").value;
+                ans = Number(ans)
+              if(temp != ans){
+                if(sum == ans ){
+                    document.getElementById("complement").innerHTML="Hurry! You are Right";
+                        r= r+1;
+                        document.getElementById("right").innerHTML = r;
+                        temp = ans 
+                }
+                else{
+                    document.getElementById("complement").innerHTML="Opps! try Again";
+                    w= w+1;
+                    document.getElementById("wrong").innerHTML = w;
+                    temp =ans 
+                }
+              }else{
+                document.getElementById("complement").innerHTML="Already Recorded";
+              }
+               
+             
+        }
